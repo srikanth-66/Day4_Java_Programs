3cbe75db24b7891953c5c74b69b54b0b5e53aea7
Srikanth_M <srikanthMidathapalli@gmail.com>
2021-07-14 14:01:33 +0530

    day4 java practise programs

---
+++ src/Day_4_Snake_Ladde_UC/Day4_SN_UC1.java
@@ -0,0 +1,13 @@
+package Day_4_Snake_Ladde_UC;
+
+public class Day4_SN_UC1 
+{
+	public static void main(String[] args) 
+	{
+	            System.out.println("HELLO WELCOME TO SNAKE AND LADDER GAME :)");
+	            System.out.println("Single Player Game is started Your at position 0.");
+	           // Console.ReadKey();
+	        }
+		
+	}
+	
---
+++ src/Day_4_Snake_Ladde_UC/Day4_SN_UC2.java
@@ -0,0 +1,10 @@
+package Day_4_Snake_Ladde_UC;
+
+import java.lang.*;
+public class Day4_SN_UC2
+{
+	public static void main(String[] args)
+	{
+
+}
+}
